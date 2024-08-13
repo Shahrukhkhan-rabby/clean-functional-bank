997cc95152b130b9bf73e62f06dbb4ba1262c806
Md. Shahrukh Khan <shahrukhkhan3418198@gmail.com>
2024-08-13 09:14:19 +0600

    deposit withdraw with function

--- functional.js
+++ functional.js
@@ -6,7 +6,36 @@ function getInputValue(fieldId){
      return value;
 
 }
+function updateTotal(fieldId, amount){
+    const totalTag = document.getElementById(fieldId);
+    const previousTotalInText = totalTag.innerText;
+    const previousTotal = parseFloat(previousTotalInText); 
+    const newTotal = previousTotal + amount;
+    totalTag.innerText = newTotal;
+}
+function updateBalance(amount, isAdding){
+    const balanceTag = document.getElementById('balance-total');
+    const balanceInText = balanceTag.innerText;
+    const previousBalance = parseFloat(balanceInText);
+    let newBalance;
+    if(isAdding == true){
+         newBalance = previousBalance + amount;
+    }
+    else{
+         newBalance = previousBalance - amount;
+    }
+    balanceTag.innerText = newBalance;
+}
 
 document.getElementById('deposit-button').addEventListener('click', function(){
     const amount = getInputValue('deposit-input');
+    updateTotal('deposit-total', amount);
+    updateBalance(amount, true);
+});
+
+// handle withdraw
+document.getElementById('withdraw-button').addEventListener('click', function(){
+    const amount = getInputValue('withdraw-input');
+    updateTotal('withdraw-total', amount);
+    updateBalance(amount, false);
 })
